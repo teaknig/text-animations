ed583aa2472f95bc2e8a251221d966d1d1b30474
Nurzhan Islambekov <teaking@bk.ru>
2022-04-23 21:25:02 +0600

    feat: init project

---
+++ webpack.config.js
@@ -0,0 +1,44 @@
+const path = require('path')
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'web'),
+    filename: 'dist/main.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              ['@babel/preset-env', { useBuiltIns: "entry" }]
+            ]
+          }
+        }
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          // Creates `style` nodes from JS strings
+          "style-loader",
+          // Translates CSS into CommonJS
+          "css-loader",
+          // Compiles Sass to CSS
+          "sass-loader",
+        ],
+      },
+    ]
+  },
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'web'),
+    },
+    compress: false,
+    port: 3000,
+  }
+}
