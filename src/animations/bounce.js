5b0447fc9df12d6ee8247fa49146b8efad9b93e2
Nurzhan Islambekov <teaking@bk.ru>
2022-04-24 00:43:51 +0600

    refactor: optimize and improve bounce animation

--- src/animations/bounce.js
+++ src/animations/bounce.js
@@ -5,24 +5,18 @@
  * @param {string} message 
  */
 const bounce = (app, element, message) => {
-  const duration = 0.4
+  const duration = 0.7
   const animation = `bounce ${duration}s cubic-bezier(.14,.99,.57,1.44) {0}`
 
-  const innerElement = message
-    .split('')
-    .map(item => `<span>${item}</span>`)
-    .join('')
-  
-  element.innerHTML = innerElement
-  const spans = element.querySelectorAll('span')
-
-  for (let i = 0; i < spans.length; i++) {
-    const item = spans[i]
+  for (let i = 0; i < message.length; i++) {
+    const item = document.createElement('span')
+    item.innerText = message[i]
     item.style.display = 'inline-block'
     item.style.animation = animation.replace('{0}', `0.${i}s`)
     item.style.opacity = 0
 
     item.addEventListener('animationstart', () => item.style.opacity = 1)
+    element.appendChild(item)
   }
 
   app.appendChild(element)
