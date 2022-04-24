d356ed4b33c71b925ff75fce2bbd0c4b4e897f7a
Nurzhan Islambekov <teaking@bk.ru>
2022-04-24 20:14:52 +0600

    feat: add second animation

--- src/animations/scale.js
+++ src/animations/scale.js
@@ -5,25 +5,26 @@
  * @param {HTMLElement} element text element 
  * @param {string} message 
  */
-const bounce = (app, main, element, message) => {
+ const scale = (app, main, element, message) => {
   const duration = 0.7
-  const animation = `bounce ${duration}s cubic-bezier(.14,.99,.57,1.44) {0}`
+  const animation = `scale ${duration}s cubic-bezier(.61,.7,.49,1.24) {0} forwards`
 
   for (let i = 0; i < message.length; i++) {
+    const delay = i / 10
+
     const item = document.createElement('span')
     item.innerText = message[i]
     item.style.display = 'inline-block'
-    item.style.animation = animation.replace('{0}', `0.${i}s`)
-    item.style.opacity = 0
+    item.style.animation = animation.replace('{0}', `${delay}s`)
 
-    item.addEventListener('animationstart', () => item.style.opacity = 1)
     element.appendChild(item)
   }
 
   app.appendChild(element)
-  element.classList.add('bounce')
+  app.classList.add('app-gragient')
+  element.classList.add('scale')
 
-  main.classList.add('background-white')
+  main.classList.add('background-black')
 }
 
-export default bounce
+export default scale
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import { shuffle } from './utils'
 
 import bounce from './animations/bounce'
+import scale from './animations/scale'
 import './style.scss'
 
 const list = [
@@ -8,12 +9,59 @@ const list = [
   'Design.',
   'Motion.',
   'Future.',
-  'Style.'
+  'Modern.'
+]
+
+const animations = [
+  bounce,
+  scale,
+  bounce,
+  scale,
+  bounce
 ]
 
 const messages = shuffle(list)
 
 const textElement = document.createElement('h1')
+const helpElement = document.createElement('p')
+const main = document.getElementById('main')
 const app = document.getElementById('app')
 
-bounce(app, textElement, messages[0])
+let index = 0
+let timer = null
+
+const init = () => {
+  animations[index](app, main, textElement, messages[index])
+
+  const reset = () => {
+    main.classList.remove(...main.classList)
+    if(main.querySelector('.help'))
+      main.removeChild(helpElement)
+      
+    textElement.classList.remove(...textElement.classList)
+    app.classList.remove(...app.classList)
+    textElement.innerHTML = ''
+    app.innerHTML = ''
+  }
+
+  const createHelp = () => {
+    helpElement.classList.add('help')
+    helpElement.innerHTML = `<span>☝</span><span>click!</span>`
+    main.appendChild(helpElement)
+  }
+
+  textElement.addEventListener('click', () => {
+    clearTimeout(timer)
+    reset()
+
+    index++
+    if(index > 4) index = 0
+
+    animations[index](app, main, textElement, messages[index])
+    timer = setTimeout(createHelp, 3000)
+  })
+
+  timer = setTimeout(createHelp, 3000)
+}
+
+init()
